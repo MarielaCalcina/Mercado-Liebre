9ce2528cd684f8572a1401d72650911217a52b5d
MarielaCalcina <marielaclacinaali@gmail.com>
2021-11-25 11:43:29 -0300

    cambios json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.static("public"));
+
 app.listen (process.env.PORT ||3000, ()=>{
     console.log('Servidor funcionando bien');
 });
